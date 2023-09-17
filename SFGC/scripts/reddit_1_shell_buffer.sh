b4c1ba50858741ff383b2bb91dfdf273694ef299
jaspal-rana <jannu.jaspal@gmail.com>
2023-09-17 15:19:03 -0500

    script changes

--- SFGC/scripts/reddit_1_shell_buffer.sh
+++ SFGC/scripts/reddit_1_shell_buffer.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 #@reddit-buff
-CUDA_VISIBLE_DEVICES=0 python buffer_inductive.py --device cuda:0 --lr_teacher 0.001 --teacher_epochs 1000 --dataset reddit --teacher_nlayers=2 --traj_save_interval=10 --param_save_interval=10 --buffer_model_type 'GCN' --num_experts=200 --wd_teacher 5e-4 --mom_teacher 0 --optim Adam --decay 0 --seed_teacher 37 >> './logs/flickr_buffer_inductive'
+CUDA_VISIBLE_DEVICES=0 python buffer_inductive.py --device cuda:0 --lr_teacher 0.001 --teacher_epochs 1000 --dataset reddit --teacher_nlayers=2 --traj_save_interval=10 --param_save_interval=10 --buffer_model_type 'GCN' --num_experts=200 --wd_teacher 5e-4 --mom_teacher 0 --optim Adam --decay 0 --seed_teacher 37 > ./logs/flickr_buffer_inductive
 
 #@reddit-r00005
-CUDA_VISIBLE_DEVICES=0 python train_coreset_inductive.py --dataset reddit --device cuda:0 --epochs 1000 --lr 0.001 --weight_decay 5e-4  --save 1 --method kcenter --reduction_rate 0.0005 --load_npy '' --seed 37 >> './logs/flickr_train_coreset_inductive_00005'
+CUDA_VISIBLE_DEVICES=0 python train_coreset_inductive.py --dataset reddit --device cuda:0 --epochs 1000 --lr 0.001 --weight_decay 5e-4  --save 1 --method kcenter --reduction_rate 0.0005 --load_npy '' --seed 37 > ./logs/flickr_train_coreset_inductive_00005
 #./logs/Coreset/reddit-reduce_0.0005-20221113-153007-643703
 
 #CUDA_VISIBLE_DEVICES=0 python train_coreset_inductive.py --dataset reddit --device cuda:0 --epochs 800 --lr_coreset 0.001 \
@@ -12,7 +12,7 @@ CUDA_VISIBLE_DEVICES=0 python train_coreset_inductive.py --dataset reddit --devi
 #--load_npy './logs/Coreset/reddit-reduce_0.0005-20221113-153007-643703' --runs 1
 
 #@reddit-r0001
-CUDA_VISIBLE_DEVICES=0 python train_coreset_inductive.py --dataset reddit --device cuda:0 --epochs 800 --lr 0.001 --weight_decay 5e-4  --save 1 --method kcenter --reduction_rate 0.001 --load_npy '' --seed 37 >> './logs/flickr_train_coreset_inductive_0001'
+CUDA_VISIBLE_DEVICES=0 python train_coreset_inductive.py --dataset reddit --device cuda:0 --epochs 800 --lr 0.001 --weight_decay 5e-4  --save 1 --method kcenter --reduction_rate 0.001 --load_npy '' --seed 37 > ./logs/flickr_train_coreset_inductive_0001
 #./logs/Coreset/reddit-reduce_0.001-20221113-150848-219047
 
 #CUDA_VISIBLE_DEVICES=0 python train_coreset_inductive.py --dataset reddit --device cuda:0 --epochs 800 --lr_coreset 0.001 \
@@ -20,12 +20,12 @@ CUDA_VISIBLE_DEVICES=0 python train_coreset_inductive.py --dataset reddit --devi
 #--load_npy './logs/Coreset/reddit-reduce_0.001-20221113-150848-219047' --runs 1
 
 #@reddit-r0002
-CUDA_VISIBLE_DEVICES=0 python train_coreset_inductive.py --dataset reddit --device cuda:0 --epochs 1000 --lr 0.001 --weight_decay 0.0005  --save 1 --method kcenter --reduction_rate 0.002 --load_npy '' --seed 37 >> './logs/flickr_train_coreset_inductive_0002'
+CUDA_VISIBLE_DEVICES=0 python train_coreset_inductive.py --dataset reddit --device cuda:0 --epochs 1000 --lr 0.001 --weight_decay 0.0005  --save 1 --method kcenter --reduction_rate 0.002 --load_npy '' --seed 37 > ./logs/flickr_train_coreset_inductive_0002
 #./logs/Coreset/reddit-reduce_0.002-20230203-205724-419628
 
 
 #@reddit-r0005
-CUDA_VISIBLE_DEVICES=0 python train_coreset_inductive.py --dataset reddit --device cuda:0 --epochs 1000 --lr 0.001 --weight_decay 5e-4  --save 1 --method kcenter --reduction_rate 0.005 --load_npy '' --seed 37 >> './logs/flickr_train_coreset_inductive_0005'
+CUDA_VISIBLE_DEVICES=0 python train_coreset_inductive.py --dataset reddit --device cuda:0 --epochs 1000 --lr 0.001 --weight_decay 5e-4  --save 1 --method kcenter --reduction_rate 0.005 --load_npy '' --seed 37 > ./logs/flickr_train_coreset_inductive_0005
 #./logs/Coreset/reddit-reduce_0.005-20221112-121718-604171
 
 #CUDA_VISIBLE_DEVICES=0 python train_coreset_inductive.py --dataset reddit --device cuda:0 --epochs 800 --lr_coreset 0.001 \
