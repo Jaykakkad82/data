44ff03794e7f26122cdeeab60174230572b13776
Jaykumar Kakkad <jay.kakkad@gmail.com>
2024-01-15 14:17:14 -0600

    script

--- Gcond_noise/scripts/run_coreset_allseeds.sh
+++ Gcond_noise/scripts/run_coreset_allseeds.sh
@@ -1,23 +1,24 @@
 
+# for feature noise
 for n in 0.1 0.15 0.2
 do
     for s in 1 15 85 120 1000
     do
         for r in 0.5 0.25 0.125
         do     
-            python train_coreset.py --dataset cora --r=${r}  --method=herding --seed=${s} --noise=${n}
-            python train_coreset.py --dataset cora --r=${r}  --method=random  --seed=${s} --noise=${n}
-            python train_coreset.py --dataset cora --r=${r}  --method=kcenter  --seed=${s} --noise=${n}
-            python train_coreset.py --dataset cora --r=${r}  --method=kmeans  --seed=${s} --noise=${n}
+            python train_coreset.py --dataset cora --r=${r}  --method=herding --seed=${s} --noise=${n} --noise_type="shuffle_nodes"
+            python train_coreset.py --dataset cora --r=${r}  --method=random  --seed=${s} --noise=${n} --noise_type="shuffle_nodes"
+            python train_coreset.py --dataset cora --r=${r}  --method=kcenter  --seed=${s} --noise=${n} --noise_type="shuffle_nodes"
+            python train_coreset.py --dataset cora --r=${r}  --method=kmeans  --seed=${s} --noise=${n} --noise_type="shuffle_nodes"
 
         done
 
         for r in 0.5 0.25 0.125
         do
-            python train_coreset.py --dataset citeseer --r=${r}  --method=herding --seed=${s} --noise=${n}
-            python train_coreset.py --dataset citeseer --r=${r}  --method=random --seed=${s} --noise=${n}
-            python train_coreset.py --dataset citeseer --r=${r}  --method=kcenter --seed=${s} --noise=${n}
-            python train_coreset.py --dataset citeseer --r=${r}  --method=kmeans --seed=${s} --noise=${n}
+            python train_coreset.py --dataset citeseer --r=${r}  --method=herding --seed=${s} --noise=${n} --noise_type="shuffle_nodes"
+            python train_coreset.py --dataset citeseer --r=${r}  --method=random --seed=${s} --noise=${n} --noise_type="shuffle_nodes"
+            python train_coreset.py --dataset citeseer --r=${r}  --method=kcenter --seed=${s} --noise=${n} --noise_type="shuffle_nodes"
+            python train_coreset.py --dataset citeseer --r=${r}  --method=kmeans --seed=${s} --noise=${n} --noise_type="shuffle_nodes"
         done
     done
 done
--- Gcond_noise/scripts/run_main.sh
+++ Gcond_noise/scripts/run_main.sh
@@ -1,16 +1,36 @@
+# for n in 0.1 0.15 0.2
+# do
+#     for s in 1000 120 85
+#     do
+#         for r in 0.25 0.5
+#         do
+#         python train_gcond_transduct.py --dataset cora --nlayers=2 --sgc=1 --lr_feat=1e-4 --gpu_id=0  --lr_adj=1e-4 --r=${r}  --seed=${s} --epoch=600 --save=1 --noise=${n}
+#         #python train_gcond_transduct.py --dataset cora --nlayers=2 --lr_feat=1e-3 --gpu_id=0 --lr_adj=1e-3 --r=${r}  --sgc=0  --seed=${s} --dis=mse --one_step=1  --epochs=5000 --save=1
+#         done
+
+#         for r in 0.25 0.5
+#         do
+#             python train_gcond_transduct.py --dataset citeseer --nlayers=2 --sgc=1 --lr_feat=1e-4 --gpu_id=0  --lr_adj=1e-4 --r=${r}  --seed=${s} --epoch=600 --save=1 --noise=${n}
+#             #python train_gcond_transduct.py --dataset citeseer --nlayers=2 --lr_feat=1e-3 --lr_adj=1e-3 --r=${r}  --sgc=0 --dis=mse --one_step=1  --epochs=3000 --save=1 --seed=${s}
+#         done
+
+#     done
+# done
+
+# feature noise
 for n in 0.1 0.15 0.2
 do
     for s in 1000 120 85
     do
         for r in 0.25 0.5
         do
-        python train_gcond_transduct.py --dataset cora --nlayers=2 --sgc=1 --lr_feat=1e-4 --gpu_id=0  --lr_adj=1e-4 --r=${r}  --seed=${s} --epoch=600 --save=1 --noise=${n}
+        python train_gcond_transduct.py --dataset cora --nlayers=2 --sgc=1 --lr_feat=1e-4 --gpu_id=0  --lr_adj=1e-4 --r=${r}  --seed=${s} --epoch=600 --save=1 --noise=${n} --noise_type="shuffle_nodes"
         #python train_gcond_transduct.py --dataset cora --nlayers=2 --lr_feat=1e-3 --gpu_id=0 --lr_adj=1e-3 --r=${r}  --sgc=0  --seed=${s} --dis=mse --one_step=1  --epochs=5000 --save=1
         done
 
         for r in 0.25 0.5
         do
-            python train_gcond_transduct.py --dataset citeseer --nlayers=2 --sgc=1 --lr_feat=1e-4 --gpu_id=0  --lr_adj=1e-4 --r=${r}  --seed=${s} --epoch=600 --save=1 --noise=${n}
+            python train_gcond_transduct.py --dataset citeseer --nlayers=2 --sgc=1 --lr_feat=1e-4 --gpu_id=0  --lr_adj=1e-4 --r=${r}  --seed=${s} --epoch=600 --save=1 --noise=${n} ----noise_type="shuffle_nodes"
             #python train_gcond_transduct.py --dataset citeseer --nlayers=2 --lr_feat=1e-3 --lr_adj=1e-3 --r=${r}  --sgc=0 --dis=mse --one_step=1  --epochs=3000 --save=1 --seed=${s}
         done
 
